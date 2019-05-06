d3e478b17e264d6d5d4b19e225a3c5d19fa32247
Ryan Parman <ryan@ryanparman.com>
2019-05-05 21:56:19 -0700

    More tweaks.

---
+++ src/Parser/ParserInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright (c) 2010–2019 Ryan Parman <http://ryanparman.com>.
+ * Copyright (c) 2016–2019 Contributors.
+ *
+ * http://opensource.org/licenses/Apache2.0
+ */
+
+declare(strict_types=1);
+
+namespace SimplePie\UtilityPack\Parser;
+
+/**
+ * The interface that all parser classes must implement and respond to.
+ */
+interface ParserInterface
+{
+}
